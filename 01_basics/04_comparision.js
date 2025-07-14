72a31f94bb64634e8bffb66e62daf85ea89c2833
prem1448 <23BCS13471@cuchd.in>
2025-07-14 11:33:10 +0000

    comparision in datatpyes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+//console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2 < 1);
+//console.log(2 == 1);
+//console.log(2 != 1);
+
+//console.log("2" > 1); //automatically "2" change into number
+//console.log("02" > 1);
+
+//avoid these type of conversion 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ==
+console.log("2" === 2);
+
