cf8d83442a3f97db56f1a8912b61fb0947d34810
prem1448 <23BCS13471@cuchd.in>
2025-08-10 15:57:33 +0000

    switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,54 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// example1
+const month = 3 
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+// exmple2
+const month1 = "march"
+
+switch (month1) {
+    case "jan":
+        console.log("January");
+        break;
+    case "fab":
+        console.log("February");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
