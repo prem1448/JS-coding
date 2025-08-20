95a91c69f66cdd11395d7d7f442ace10ac1de51f
prem1448 <23BCS13471@cuchd.in>
2025-08-20 10:51:53 +0000

    class tutorial

---
+++ class/one.js
@@ -0,0 +1,45 @@
+var str = "javascript"
+var strn = "js is good for development"
+// console.log(str.length);
+// console.log(str.charAt(3));
+// console.log(str.concat(" is king"));
+// console.log(str.charCodeAt(3));
+// console.log(str.split(""));
+// console.log(strn.split(""));
+// console.log(str.split(" "));
+// console.log(strn.split(" "));
+// console.log(str.includes("c++"));
+// console.log(strn.indexOf("js"));
+// console.log(strn.lastIndexOf("js"));
+// console.log(strn.lastIndexOf("development"));
+// console.log(strn.replaceAll("js", "c++"));
+// console.log(str.slice(0,3));
+// console.log(typeof null);
+// console.log(typeof undefined);
+var arr = [1,2,3,4,5,6,7]
+// arr.push(8)
+// console.log(arr);
+// arr.pop()
+// console.log(arr);
+// arr.unshift(0)
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+// console.log(arr.toString());
+// console.log(arr.join(" "));
+// console.log(arr.join(" and "));
+// console.log(arr.slice(0,3));
+// console.log(arr.splice(1,3));
+arr.forEach( function (item) {
+    console.log(item); 
+} )
+arr.forEach( (item) => {
+    console.log(item); 
+} )
+// map
+// arrow
+// foreach
+
+
+
+
